13cb237340c15b8251ce54da6f0f90dae569c80b
ziggybuilds <ellisjoshuag@gmail.com>
2018-06-28 12:32:23 -0400

    Fixed sharing url encoding

--- inc/share-fb-tw.php
+++ inc/share-fb-tw.php
@@ -1,11 +1,23 @@
-	<?php 
+<?php
+/**
+*	Post share wrapper
+*	ACF theme options controls the hashtag, will auto share the home url
+*/
+?>
+<?php 
 	//Begin Social Profile Loop this while render as <a></a>
+	if ( get_field('hashtag', 'acf-options') ):
+		$tweetTag = "&hashtags=" . get_field('hashtag', 'acf-options');
+	else :
+		$tweetTag = "&hashtags=";
+	endif;
+	
 	$twitter = "https://twitter.com/intent/tweet?url=";
 	$fb = "https://www.facebook.com/sharer/sharer.php?u="; 
-	$homeLink = urlencode( get_home_url() );
-	?>
+	$postLink = urlencode( get_home_url() );
+?>
 
-	<div class="share-wrapper">
-			<a href="https://twitter.com/share" class="twitter-share-button" data-show-count="false" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-			<a class="facebook" href="http://www.facebook.com/sharer.php?u=<?php $homeLink ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-	</div>
+<div class="share-wrapper">
+	<a href="<?php echo $twitter; echo $postLink; echo $tweetTag; ?>" class="twitter-share-button" data-show-count="false" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+	<a class="facebook" href="<?php echo $fb; echo $postLink; ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+</div>
